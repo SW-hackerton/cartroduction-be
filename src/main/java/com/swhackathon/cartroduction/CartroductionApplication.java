be8ab0e32dbc8e48259eb5d765d5e61275b3cbb3
Yunkeun <yunx2@naver.com>
2022-06-22 22:11:24 +0900

    chore: init spring backend project

---
+++ src/main/java/com/swhackathon/cartroduction/CartroductionApplication.java
@@ -0,0 +1,13 @@
+package com.swhackathon.cartroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swhackathon/cartroduction/CartroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swhackathon.cartroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
