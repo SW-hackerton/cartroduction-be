b484693c8e6a51a3e50d03da5b091c52dff1b62c
Yunkeun <yunx2@naver.com>
2022-06-23 05:56:51 +0900

    fix: 등록 전체 조회 기능 구현

--- src/main/java/com/swhackathon/cartroduction/domain/registration/controller/RegistrationController.java
+++ src/main/java/com/swhackathon/cartroduction/domain/registration/controller/RegistrationController.java
@@ -2,11 +2,15 @@ package com.swhackathon.cartroduction.domain.registration.controller;
 
 import com.swhackathon.cartroduction.domain.registration.domain.entity.Registration;
 import com.swhackathon.cartroduction.domain.registration.dto.RegistrationRequest;
+import com.swhackathon.cartroduction.domain.registration.dto.RegistrationResponse;
 import com.swhackathon.cartroduction.domain.registration.service.RegistrationService;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +22,17 @@ public class RegistrationController {
 	public final RegistrationService registrationService;
 
 	@PostMapping("/manager/register")
-	public ResponseEntity<Registration> register(@Validated @RequestBody RegistrationRequest request) {
+	public ResponseEntity<Registration> register(
+		@Validated @RequestBody RegistrationRequest request) {
 		Registration registration = request.toEntity();
 		return new ResponseEntity<>(registrationService.save(registration), HttpStatus.CREATED);
+	}
+
+	@GetMapping()
+	public ResponseEntity<List<Registration>> findAll() {
+		return ResponseEntity.ok()
+			.body(registrationService.findAll().stream()
+				.map(RegistrationResponse::of)
+				.collect(Collectors.toList()));
 	}
 }
--- src/main/java/com/swhackathon/cartroduction/domain/registration/dto/RegistrationResponse.java
+++ src/main/java/com/swhackathon/cartroduction/domain/registration/dto/RegistrationResponse.java
@@ -1,11 +1,47 @@
 package com.swhackathon.cartroduction.domain.registration.dto;
 
+import com.swhackathon.cartroduction.domain.registration.domain.entity.Registration;
+import com.swhackathon.cartroduction.domain.registration.domain.enumeration.Category;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegistrationResponse {
 
+	private String managerName;
 
+	private Category category;
+
+	private String content;
+
+	private String price;
+
+	private String carNumber;
+
+	private String carDistance;
+
+	private String estimatesImageUrl;
+
+	private String carImageUrl;
+
+	private LocalDateTime date;
+
+	public static Registration of(Registration registration) {
+		return Registration.builder()
+			.id(registration.getId())
+			.managerName(registration.getManagerName())
+			.category(registration.getCategory())
+			.content(registration.getContent())
+			.price(registration.getPrice())
+			.carNumber(registration.getCarNumber())
+			.carDistance(registration.getCarDistance())
+			.estimatesImageUrl(registration.getEstimatesImageUrl())
+			.carImageUrl(registration.getCarImageUrl())
+			.date(registration.getDate())
+			.build();
+	}
 }
